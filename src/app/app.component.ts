1296fe48a18e46c8116c174bcaec7de3972ebc02
Nawel BP <nawel.berqouch@gmail.com>
2023-04-27 15:16:30 +0200

    fuite de memoire, observable infini

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,22 +12,23 @@ export class AppComponent implements OnInit {
   yellowTrainsCalled = 0;
 
   ngOnInit() {
-    interval(500).pipe(
+   /*  interval(500).pipe(
       take(10),
       map(value => value % 2 === 0 ? 'rouge' : 'jaune'),
       tap(color => console.log(`La lumière s'allume en %c${color}`, `color: ${this.translateColor(color)}`)),
-      /* 
-      mergeMap : assure la mise en parallèle : l'Observable extérieur peut souscrire aux Observables intérieurs suivants sans attendre que les précédents soient complétés. 
-      concatMap : assure la mise en série : il attend que les Observables intérieurs complètent avant de souscrire aux suivants– même si l'Observable extérieur émet plusieurs fois. Les Observables intérieurs seront traités en séquence à la suite.
-      exhaustMap : assure le traitement complet d'une souscription avant d'observer une nouvelle émission de l'Observable extérieur. Si d’autres demandes sont faites entre temps, elles ne seront pas prises en compte. 
-      switchMap : traite la dernière demande de souscription de l’Observable extérieur et annule toute souscription précédente non-complétée
-      */ 
+       
+      //mergeMap : assure la mise en parallèle : l'Observable extérieur peut souscrire aux Observables intérieurs suivants sans attendre que les précédents soient complétés. 
+      //concatMap : assure la mise en série : il attend que les Observables intérieurs complètent avant de souscrire aux suivants– même si l'Observable extérieur émet plusieurs fois. Les Observables intérieurs seront traités en séquence à la suite.
+      //exhaustMap : assure le traitement complet d'une souscription avant d'observer une nouvelle émission de l'Observable extérieur. Si d’autres demandes sont faites entre temps, elles ne seront pas prises en compte. 
+      //switchMap : traite la dernière demande de souscription de l’Observable extérieur et annule toute souscription précédente non-complétée
+       
       mergeMap(color => this.getTrainObservable$(color)),
       tap(train => console.log(`Train %c${train.color} ${train.trainIndex} arrivé !`, `font-weight: bold; color: ${this.translateColor(train.color)}`))
     ).subscribe();
+    */
   }
 
-  getTrainObservable$(color: 'rouge' | 'jaune') {
+  /* getTrainObservable$(color: 'rouge' | 'jaune') {
     const isRedTrain = color === 'rouge';
     isRedTrain ? this.redTrainsCalled++ : this.yellowTrainsCalled++;
     const trainIndex = isRedTrain ? this.redTrainsCalled : this.yellowTrainsCalled;
@@ -39,6 +40,6 @@ export class AppComponent implements OnInit {
 
   translateColor(color: 'rouge' | 'jaune') {
     return color === 'rouge' ? 'red' : 'yellow';
-  }
-  
+  }  */
+
 }
--- src/app/snap-face-list/snap-face-list.component.ts
+++ src/app/snap-face-list/snap-face-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
 import { FaceSnapService } from '../services/face-snap.service';
+import { interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-snap-face-list',
@@ -14,5 +15,14 @@ export class SnapFaceListComponent implements OnInit {
 
   ngOnInit(): void {
   this.faceSnaps=this._facesnapService.getAllFaceSnaps();
+
+  /*
+    Tests de fuite de mémoire
+    Créer plusieurs instance de l'observable 
+    Observer la console -> probleme si observable renvoie des tableaux, ou objects volumineux
+  */
+  interval(1000).pipe(
+    tap(console.log)
+    ).subscribe();
   }
 }
