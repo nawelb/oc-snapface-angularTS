662d18335be37772f16cc49b4a4a5fff3b4792fc
Nawel BP <nawel.berqouch@gmail.com>
2023-04-27 14:34:07 +0200

    part1 observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Observable, filter, interval, map } from 'rxjs';
+import { Observable, filter, interval, map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -14,9 +14,15 @@ export class AppComponent implements OnInit {
   ngOnInit(){
     this.interval$ = interval(1000).pipe(
       filter(value=> value%3===0),
-      map(value=>value%2===0 ? value+" paire" : value+" impaire")
+      map(value=>value%2===0 ? value+" paire" : value+" impaire"),
+      tap(text => this.logger(text))
     ); 
   }
 
 
+
+  logger(text:string){
+    // pas de modification de l'observable, add "log :"
+    console.log(`log :  ${text}`);
+  }
 }
