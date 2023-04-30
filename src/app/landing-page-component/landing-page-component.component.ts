08f0d3fabee4426cc9dd8d9389964e754324efd8
Nawel BP <nawel.berqouch@gmail.com>
2023-04-30 15:43:17 +0200

    recover complet form with #..=ngForm

--- src/app/landing-page-component/landing-page-component.component.ts
+++ src/app/landing-page-component/landing-page-component.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,6 +9,7 @@ import { Router } from '@angular/router';
 })
 export class LandingPageComponentComponent implements OnInit {
   userEmailTS!:string;
+  userNameTS!:string;
   constructor(private router:Router) { }
 
 
@@ -18,7 +20,7 @@ export class LandingPageComponentComponent implements OnInit {
     this.router.navigateByUrl('facesnaps')
   }
 
-  onSubmit(){
-    console.log(this.userEmailTS);
+  onSubmit(form:NgForm){
+    console.log(form.value);
   }
 }
