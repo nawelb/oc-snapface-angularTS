77093be1f3f7f155e322027119e3f997bee1351f
Nawel BP <nawel.berqouch@gmail.com>
2023-04-30 14:42:18 +0200

    destroy observable

--- src/app/snap-face-list/snap-face-list.component.ts
+++ src/app/snap-face-list/snap-face-list.component.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
 import { FaceSnapService } from '../services/face-snap.service';
-import { interval, take, tap } from 'rxjs';
+import { Subject, interval, take, takeUntil, tap } from 'rxjs';
 
 @Component({
   selector: 'app-snap-face-list',
   templateUrl: './snap-face-list.component.html',
   styleUrls: ['./snap-face-list.component.scss']
 })
-export class SnapFaceListComponent implements OnInit {
+export class SnapFaceListComponent implements OnInit, OnDestroy {
 
   faceSnaps!:FaceSnap[];
+  private destroy$!:Subject<boolean>;
   constructor( private _facesnapService : FaceSnapService) { }
 
-  ngOnInit(): void {
-  this.faceSnaps=this._facesnapService.getAllFaceSnaps();
+   ngOnInit(): void {
+    this.destroy$ = new Subject<boolean>();
+    this.faceSnaps=this._facesnapService.getAllFaceSnaps();
 
   /*
     Tests de fuite de mémoire
@@ -22,9 +24,16 @@ export class SnapFaceListComponent implements OnInit {
     Observer la console -> probleme si observable renvoie des tableaux, ou objects volumineux
   */
   interval(1000).pipe(
+  
     //opérateur take dénombre le nombre d'évenement à observer avant d'arreter l'observable pour éviter les fuites de mémoire -> ici 3
-    take(3),
-    tap(console.log)
+    //take(3),
+    tap(console.log),
+    takeUntil(this.destroy$)
     ).subscribe();
   }
+
+  ngOnDestroy(): void {
+   this.destroy$.next(true);
+  }
+
 }
